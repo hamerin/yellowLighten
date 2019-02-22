acdc410b0223bd5b747e525465e00b8dd9c99f0b
himyu <himyuuuu1@gmail.com>
2019-02-23 03:50:09 +0900

    Test

--- app_start.py
+++ app_start.py
@@ -1,9 +1,7 @@
 from venv import client
-from boto.s3.connection import S3Connection
 import os
 
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
-TOKEN = ''
+TOKEN = os.environ['DISCORD_TOKEN']
 
 if __name__ == '__main__':
     client.run(TOKEN)
--- venv/__init__.py
+++ venv/__init__.py
@@ -1,17 +1,30 @@
 # Work with Python 3.6
 import discord
 import asyncio
+import pymongo
+import os
 
+mclient = pymongo.MongoClient("mongodb+srv://admin:{PASSWORD}@yellowlighten-wukhb.mongodb.net/test?retryWrites=true"
+                             .format(PASSWORD=os.environ['MONGODB_TOKEN']))
 client = discord.Client()
+settings_db = mclient.settings
+
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
+        settings_db[message.server.id].insert_one({'prefix': '!'})
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('!square'):
+        msg=''
+        msg_get=message.content.split()
+        if len(msg_get) != 2:
+            msg='Input invaild'
+        else:
+            msg='Square of {num} is {sq}'.format(num=int(msg_get[1]), sq=int(msg_get[1])**2)
         await client.send_message(message.channel, msg)
 
 @client.event
