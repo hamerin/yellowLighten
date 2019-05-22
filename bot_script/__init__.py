5a674b84447693c0db4acf36997e8a7f78fa9e2c
himyu <himyuuuu1@gmail.com>
2019-05-22 21:29:20 +0900

    code optimalization & neko command

--- bot_script/__init__.py
+++ bot_script/__init__.py
@@ -1,4 +1,4 @@
-# Work with Python 3.6
+# Work with python 3.6
 import discord
 import asyncio
 import pymongo
@@ -6,6 +6,7 @@ import os
 import copy
 import random
 import requests
+import nekos
 from bs4 import BeautifulSoup
 from bson.objectid import ObjectId
 from . import pokemon
@@ -19,6 +20,26 @@ settings_db = mclient.settings
 
 @client.event
 async def on_message(message):
+  async def snd_embed(embed):
+    await client.send_message(message.channel, embed=embed)
+
+  async def snd_msg(message, description=None, author=None, author_url=None):
+    embed = discord.Embed(title=message,
+                          description=description,
+                          colour=color)
+    if author:
+      if author_url:
+        embed.set_author(name=author, icon_url=author_url)
+      else:
+        embed.set_author(name=author)
+
+    await snd_embed(embed)
+
+  async def snd_text(text):
+    await client.send_message(message.channel, text)
+
+  async def raise_err(description=None, title="잘못된 입력입니다."):
+    await snd_msg(title, description)
 
   if message.author == client.user:
     return
@@ -35,37 +56,10 @@ async def on_message(message):
   if not message.content.startswith(prefix):
     return
 
-  async def snd_embed(__embed):
-    await client.send_message(message.channel, embed=__embed)
-
-  async def snd_msg(__message, __description=None, __author=None, __author_url=None):
-    _embed = discord.Embed(title=__message,
-                           description=__description,
-                           colour=color)
-    if __author:
-      if __author_url:
-        _embed.set_author(name=__author, icon_url=__author_url)
-      else:
-        _embed.set_author(name=__author)
-
-    await snd_embed(_embed)
-
-  async def snd_text(__text):
-    await client.send_message(message.channel, __text)
-
-  async def raise_err(__description=None, __title="잘못된 입력입니다."):
-    await snd_msg(__title, __description)
-
-  def factorize(n):
-    while n > 1:
-      for i in range(2, n + 1):
-        if n % i == 0:
-          n //= i
-          yield i
-          break
-
   parsedContent = message.content[len(prefix):]
   command = parsedContent.split()[0]
+  actualContent = None if len(parsedContent.split(
+      ' ', 1)) < 2 else parsedContent.split(' ', 1)[1]
 
   if command == 'config':
     if message.server.id in settings_db.collection_names():
@@ -125,29 +119,28 @@ async def on_message(message):
     return
 
   elif command == 'color':
-    msg_get = parsedContent.split(' ', 1)
-    if len(msg_get) < 2:
+    if not actualContent:
       await raise_err()
       return
-    elif not len(msg_get[1]) == 6:
+    elif not len(actualContent) == 6:
       await raise_err()
       return
     try:
-      if msg_get[1] == 'random':
-        __color = random.randint(0x000000, 0xffffff)
+      if actualContent == 'random':
+        changedColor = random.randint(0x000000, 0xffffff)
       else:
-        __color = int(msg_get[1], 16)
+        changedColor = int(actualContent, 16)
 
       settings = copy.deepcopy(_settings)
       settings_id = settings['_id']
       settings.pop('_id', None)
-      settings['color'] = __color
+      settings['color'] = changedColor
       settings_db[message.server.id].replace_one(
           {'_id': settings_id}, settings)
 
       await snd_embed(
           discord.Embed(title="색 설정 완료",
-                        colour=__color))
+                        colour=changedColor))
       return
 
     except SyntaxError:
@@ -172,60 +165,57 @@ async def on_message(message):
     return
 
   elif command == 'calc':
-    msg_get = parsedContent.split(' ', 1)
-    if len(msg_get) < 2:
+    if not actualContent:
       await raise_err()
       return
     else:
       try:
-        if not utils.check_vaild(msg_get[1], '0123456789+-x^/()'):
+        if not utils.check_vaild(actualContent, '0123456789+-x^/()'):
           await raise_err('0123456789+-x^/()만 포함될 수 있습니다.')
           return
         else:
-          msg = '{}'.format(
-              eval(msg_get[1].replace('x', '*').replace('^', '**')))
+          sendingMessage = '{}'.format(
+              eval(actualContent.replace('x', '*').replace('^', '**')))
       except SyntaxError:
         await raise_err()
         return
-    await snd_msg("계산 결과", msg)
+    await snd_msg("계산 결과", sendingMessage)
     return
 
   elif command == 'factor':
-    msg_get = parsedContent.split(' ', 1)
-    if len(msg_get) < 2:
+    if not actualContent:
       await raise_err()
       return
     else:
-      if not utils.check_vaild(msg_get[1], '0123456789'):
-        await raise_err('2 이상 10000000 이하의 정수만 소인수분해할 수 있습니다.')
+      if not utils.check_vaild(actualContent, '0123456789+-x^/()'):
+        await raise_err('0123456789+-x^/()만 포함될 수 있습니다.')
         return
       else:
-        num = int(msg_get[1])
+        num = int(actualContent)
         if num > 10 ** 7 or num < 2:
           await raise_err('2 이상 10000000 이하의 정수만 소인수분해할 수 있습니다.')
           return
         else:
           factor_dict = {}
-          for factor in factorize(num):
+          for factor in utils.factorize(num):
             if factor in factor_dict:
               factor_dict[factor] += 1
             else:
               factor_dict.update({factor: 1})
-          msg = "{}".format(' x '.join([(str(key) if value == 1 else '{}^{}'.format(
+          sendingMessage = "{}".format(' x '.join([(str(key) if value == 1 else '{}^{}'.format(
               key, value)) for key, value in factor_dict.items()]))
-    await snd_msg("소인수분해 결과", msg)
+    await snd_msg("소인수분해 결과", sendingMessage)
     return
 
-  elif command == 'wiki' or command == 'wiki-ko':
-    msg_get = parsedContent.split(' ', 1)
-    image_url = "https://store-images.s-microsoft.com/image/apps.41885.9007199266246789.b5c9bced-c132-42c7-b8d5-8ae95a968b20.9605e2c4-06d4-46b9-8cfc-2e430b5bcab0"
-    if len(msg_get) < 2:
+  elif command in ('wiki', 'wiki-ko'):
+    favicon_url = "https://store-images.s-microsoft.com/image/apps.41885.9007199266246789.b5c9bced-c132-42c7-b8d5-8ae95a968b20.9605e2c4-06d4-46b9-8cfc-2e430b5bcab0"
+    if not actualContent:
       await raise_err()
       return
     else:
       try:
         url = 'https://ko.wikipedia.org/wiki/{}'.format(
-            msg_get[1].replace(' ', '_'))
+            actualContent.replace(' ', '_'))
         req = requests.get(url)
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
@@ -233,22 +223,21 @@ async def on_message(message):
         desc = soup.select(
             'div.mw-parser-output > p:not(.mw-empty-elt)')[0].text[:-1] + '\n\n' + url
       except IndexError:
-        await snd_msg(None, "없는 문서입니다!", "Wikipedia-ko", image_url)
+        await snd_msg(None, "없는 문서입니다!", "Wikipedia-ko", favicon_url)
         return
 
-    await snd_msg(title, desc, "Wikipedia-ko", image_url)
+    await snd_msg(title, desc, "Wikipedia-ko", favicon_url)
     return
 
   elif command == 'wiki-en':
-    msg_get = parsedContent.split(' ', 1)
-    image_url = "https://store-images.s-microsoft.com/image/apps.41885.9007199266246789.b5c9bced-c132-42c7-b8d5-8ae95a968b20.9605e2c4-06d4-46b9-8cfc-2e430b5bcab0"
-    if len(msg_get) < 2:
+    favicon_url = "https://store-images.s-microsoft.com/image/apps.41885.9007199266246789.b5c9bced-c132-42c7-b8d5-8ae95a968b20.9605e2c4-06d4-46b9-8cfc-2e430b5bcab0"
+    if not actualContent:
       await raise_err()
       return
     else:
       try:
         url = 'https://en.wikipedia.org/wiki/{}'.format(
-            msg_get[1].replace(' ', '_'))
+            actualContent.replace(' ', '_'))
         req = requests.get(url)
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
@@ -256,23 +245,23 @@ async def on_message(message):
         desc = soup.select(
             'div.mw-parser-output > p:not(.mw-empty-elt)')[0].text[:-1] + '\n\n' + url
       except IndexError:
-        await snd_msg(None, "없는 문서입니다!", "Wikipedia-en", image_url)
+        await snd_msg(None, "없는 문서입니다!", "Wikipedia-en", favicon_url)
         return
 
-    await snd_msg(title, desc, "Wikipedia-en", image_url)
+    await snd_msg(title, desc, "Wikipedia-en", favicon_url)
+    return
 
-  elif command == 'boj' or command == 'baekjoon':
-    msg_get = parsedContent.split(' ', 1)
-    image_url = "https://pbs.twimg.com/profile_images/804340136259428353/fZud08Ao_400x400.jpg"
-    if len(msg_get) < 2:
+  elif command in ('boj', 'baekjoon'):
+    favicon_url = "https://pbs.twimg.com/profile_images/804340136259428353/fZud08Ao_400x400.jpg"
+    if not actualContent:
       await raise_err()
       return
-    if not utils.check_vaild(msg_get[1], '0123456789'):
+    if not utils.check_vaild(actualContent, '0123456789'):
       await raise_err('문제 번호는 정수여야 합니다.')
       return
     else:
       try:
-        url = 'https://www.acmicpc.net/problem/{}'.format(msg_get[1])
+        url = 'https://www.acmicpc.net/problem/{}'.format(actualContent)
         req = requests.get(url)
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
@@ -281,33 +270,45 @@ async def on_message(message):
         inp = soup.select('#problem_input')[0].text[1:-1]
         outp = soup.select('#problem_output')[0].text[1:-1]
       except IndexError:
-        await snd_msg(None, "없는 문제입니다!", "Baekjoon Online Judge", image_url)
+        await snd_msg(None, "없는 문제입니다!", "Baekjoon Online Judge", favicon_url)
         return
 
     embed = discord.Embed(title=title,
                           colour=color)
     embed.set_author(name="Baekjoon Online Judge",
-                     icon_url=image_url)
+                     icon_url=favicon_url)
     embed.add_field(name="문제", value=desc, inline=False)
     embed.add_field(name="입력", value=inp, inline=False)
     embed.add_field(name="출력", value=outp + '\n\n' + url, inline=False)
     await snd_embed(embed)
+    return
 
-  elif command == 'pokedex' or command == 'poke':
-    msg_get = parsedContent.split(' ', 1)
-    if len(msg_get) < 2:
+  elif command == 'neko':
+    favicon_url = "https://avatars2.githubusercontent.com/u/34457007?s=200&v=4"
+    image_url = nekos.img("neko")
+    embed = discord.Embed(colour=color)
+    embed.set_author(name="nekos.life",
+                     icon_url=favicon_url)
+    embed.set_image(url=image_url)
+    await snd_embed(embed)
+    return
+
+  elif command in ('pokedex', 'poke'):
+    if not actualContent:
       await raise_err("포켓몬의 영어 이름, 한글 이름 또는 전국도감 번호를 입력해 주세요.")
       return
 
-    find_resp = pokemon.find(msg_get[1])
-    if not find_resp[0]:
+    findResponse = pokemon.find(actualContent)
+    if not findResponse[0]:
       await raise_err("없는 포켓몬입니다.")
       return
     else:
-      await snd_msg("찾기 결과", '{}'.format(find_resp[1]))
+      await snd_msg("찾기 결과", '{}'.format(findResponse[1]))
+      return
 
   else:
     await snd_msg("없는 명령어", '명령어 리스트는 `{}help` 를 통해 확인해 주세요!'.format(prefix))
+    return
 
 
 @client.event
--- bot_script/utils.py
+++ bot_script/utils.py
@@ -4,3 +4,11 @@ def check_vaild(__str, __allowed):
     if i not in __allowed:
       _flg = False
   return _flg
+
+def factorize(n):
+  while n > 1:
+    for i in range(2, n + 1):
+      if n % i == 0:
+        n //= i
+        yield i
+        break
